2d4d1fd0136711344d3d5845c68cfb23f1d588dd
Dobrovolsky777 <itskreydpvp1@gmail.com>
2025-04-08 18:06:56 +0300

    Добавил файл _redirects и исправил конфиг

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 import { fileURLToPath, URL } from 'node:url';
 
 export default defineConfig({
+  base: '/', // <-- обязательно, если на Netlify. Если GitHub Pages — укажи /my-door-configurator/
   plugins: [react()],
   resolve: {
     alias: {
